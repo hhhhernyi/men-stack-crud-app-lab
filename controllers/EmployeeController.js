93328690f7d3cdf45473fd337980ff9e12a52f65
Hern Yi Lee <hyileenet@gmail.com>
2025-02-09 23:01:35 +0800

    added oontrollers and add some styling

--- controllers/EmployeeController.js
+++ controllers/EmployeeController.js
@@ -1,4 +1,62 @@
-const create =async (req, res) => {
-   console.log(req.body)
-     res.send('employee created');
-  }
+const Employee = require("../models/Employee");
+
+const home = async (req, res) => {
+  res.render("./home/index.ejs");
+};
+
+const index = async (req, res) => {
+  const allEmployees = await Employee.find({});
+  console.log("all employees: ", allEmployees);
+  res.render("./employee/index.ejs", { allEmployees });
+};
+const showCreateForm = async (req, res) => {
+  res.render("./employee/new.ejs");
+};
+
+const show = async (req, res) => {
+  const employeeId = req.params.employeeId;
+  const oneEmployee = await Employee.findById(employeeId);
+  console.log("one employees: ", oneEmployee);
+  res.render("./employee/details.ejs", { oneEmployee });
+};
+const create = async (req, res) => {
+  console.log(req.body);
+  const newEmployee = await Employee.create(req.body);
+  console.log("New employee: ", newEmployee);
+  res.redirect("/employee");
+};
+const showUpdateForm = async (req, res) => {
+  const employeeId = req.params.employeeId;
+  const oneEmployee = await Employee.findById(employeeId);
+  console.log("Employee to be updated: ", oneEmployee);
+  res.render("./employee/update.ejs", { oneEmployee });
+};
+const update = async (req, res) => {
+  const updateId = req.params.employeeId;
+  const updateDetails = req.body;
+  console.log("update details: ", updateDetails);
+  const updatedEmployee = await Employee.findByIdAndUpdate(
+    updateId,
+    updateDetails,
+    { new: true }
+  );
+  console.log("Updated details: ", updatedEmployee);
+  //res.redirect("/employee");
+  res.redirect("/employee");
+};
+const del = async (req, res) => {
+  const deleteId = req.params.employeeId;
+  await Employee.findByIdAndDelete(deleteId);
+  res.redirect("/employee");
+};
+
+module.exports = {
+  index,
+  show,
+  create,
+  update,
+  del,
+  home,
+  showCreateForm,
+  showUpdateForm,
+};
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const methodOverride = require("method-override");
 const path = require("path");
 const morgan = require("morgan")
 const Employee = require('./models/Employee')
+const EmployeeController = require('./controllers/EmployeeController')
 
 // CONFIG
 dotenv.config(); // Loads the environment variables from .env file
@@ -26,63 +27,15 @@ app.get("/", async (req, res) => {
     res.render("index.ejs");
   });
 
-// home page
-app.get("/home", async (req, res) => {
-    res.render("./home/index.ejs");
-  });
+app.get("/home", EmployeeController.home); // show home page
+app.get("/employee", EmployeeController.index); // show all employees
+app.get("/employee/new", EmployeeController.showCreateForm); // show form to create new employee
+app.get("/employee/:employeeId", EmployeeController.show); // show the details of one employee
+app.post("/employee",EmployeeController.create ); // method to create new employee
+app.get("/employee/:employeeId/edit",EmployeeController.showUpdateForm  ); // show the form to update a employee
+app.put("/employee/:employeeId", EmployeeController.update); // method to update an employee
+app.delete("/employee/:employeeId", EmployeeController.del); // method to delete employee
 
-// show all employees
-app.get("/employee", async (req, res) => {
-    const allEmployees = await Employee.find({})
-    console.log('all employees: ',allEmployees)
-    res.render("./employee/index.ejs", {allEmployees});
-  });
-
- // show form to create new employee
-app.get("/employee/new", async (req, res) => {
-    res.render("./employee/new.ejs");
-  }); 
-
-// show the details of one employee
-app.get("/employee/:employeeId", async (req, res) => {
-    const employeeId = req.params.employeeId;
-    const oneEmployee = await Employee.findById(employeeId)
-    console.log('one employees: ',oneEmployee)
-    res.render("./employee/details.ejs", {oneEmployee});
-  });
-
-// method to create new employee
-app.post("/employee", async (req, res) => {
-    console.log(req.body)
-    const newEmployee = await Employee.create(req.body)
-    console.log('New employee: ', newEmployee)
-    res.redirect('/employee')
-   });
-// show the form to update a employee
-app.get("/employee/:employeeId/edit", async (req, res) => {
-    const employeeId = req.params.employeeId;
-    const oneEmployee = await Employee.findById(employeeId)
-    console.log('Employee to be updated: ',oneEmployee)
-    res.render("./employee/update.ejs", {oneEmployee});
-  });
-app.put("/employee/:employeeId", async (req,res)=>{
-    const updateId = req.params.employeeId
-    const updateDetails = req.body
-    console.log('update details: ',updateDetails)
-    const updatedEmployee = await Employee.findByIdAndUpdate(updateId,
-        updateDetails,
-        {new: true})
-    console.log('Updated details: ', updatedEmployee)
-    //res.redirect("/employee");
-    res.redirect('/employee')
-  });
-// method to delete employee
-app.delete("/employee/:employeeId", async (req,res)=>{
-    const deleteId = req.params.employeeId
-    await Employee.findByIdAndDelete(deleteId)
-    res.redirect("/employee");
-  });
-// method to update an employee
 
 // LISTEN
 app.listen(3000, () => {
